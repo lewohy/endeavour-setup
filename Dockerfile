837b9acf61c2eb8865862c48cd506d8c3c4112f7
lewohy <lwh8762@gmail.com>
2024-09-19 18:53:49 +0900

    pacman.conf 수정

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,65 @@ WORKDIR /home/lewohy
 
 RUN git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 
+RUN yay -S --noconfirm reflector
+RUN sudo reflector --verbose -c KR --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+RUN sudo cat /etc/pacman.d/mirrorlist
+RUN sudo sed -i 's/#?ParallelDownloads = \d+/ParallelDownloads = 8/g' /etc/pacman.conf
+
 # Update
 RUN yay -Syu --noconfirm
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+RUN yay -S --noconfirm \
+    keyd \
+    gdm \
+    google-chrome \
+    obsidian \
+    discord \
+    fish \
+    ttf-jetbrains-mono-nerd \
+    tofi \
+    eww \
+    bat \
+    kitty \
+    fd \
+    fzf \
+    lsd \
+    neovim \
+    neovide \
+    ripgrep \
+    starship \
+    zoxide \
+    croc \
+    curl \
+    dua-cli \
+    duf \
+    procs \
+    tre-command \
+    neofetch \
+    fastfetch \
+    bottom \
+    git-delta \
+    sd \
+    tealdeer \
+    rslsync \
+    kime-git \
+    jetbrains-toolbox \
+    go \
+    # hyprland-git \
+    # hyprlock-git \
+    # xdg-desktop-portal-hyprland-git \
+    # swaync-git \
+    cliphist \
+    playerctl-git \
+    grim-git \
+    slurp-git \
+    jq \
+    wf-recorder-git \
+    brightnessctl-git \
+    # wine-stable \
+    telegram-desktop \
+    figma-linux-bin \
+    notion-app-electron
 
 # setup.sh
 COPY --chown=lewohy:lewohy setup.sh /home/lewohy/setup.sh
--- setup.sh
+++ setup.sh
@@ -26,8 +26,9 @@ cd ~
 
 # Add mirrorlist
 center_text "Add mirrorlist"
+yay -S --noconfirm reflector
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose -c KR --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 center_text "/etc/pacman.d/mirrorlist"
 sudo cat /etc/pacman.d/mirrorlist
 center_text ""
@@ -51,163 +52,70 @@ center_text "Updating firmware"
 yay -S --noconfirm linux-firmware
 write_log "Done"
 
-# Install 
-center_text "Install packages"
-
-write_log "github-cli"
+# Install github0-cli
+center_text "github-cli"
 yay -S --noconfirm github-cli
 gh auth login -p https -w
+write_log "Done"
 
-write_log "keyd"
-yay -S --noconfirm keyd
+# Install Rust
+center_text "Install Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+write_log "Done"
 
-write_log "gdm"
-yay -S --noconfirm gdm
-sudo systemctl enable gdm.service
-
-write_log "google-chrome"
-yay -S --noconfirm google-chrome
-
-write_log "obsidian"
-yay -S --noconfirm obsidian
-
-write_log "discord"
-yay -S --noconfirm discord
-
-write_log "fish"
-yay -S --noconfirm fish
-
-write_log "ttf-jetbrains-mono-nerd"
-yay -S --noconfirm ttf-jetbrains-mono-nerd
-
-write_log "tofi"
-yay -S --noconfirm tofi
-
-write_log "eww"
-yay -S --noconfirm eww
-
-write_log "bat"
-yay -S --noconfirm bat
-
-write_log "kitty"
-yay -S --noconfirm kitty
-
-write_log "fd"
-yay -S --noconfirm fd
-
-write_log "fzf"
-yay -S --noconfirm fzf
-
-write_log "lsd"
-yay -S --noconfirm lsd
-
-write_log "neovim"
-yay -S --noconfirm neovim
-
-write_log "neovide"
-yay -S --noconfirm neovide
-
-write_log "ripgrep"
-yay -S --noconfirm ripgrep
-
-write_log "starship"
-yay -S --noconfirm starship
-
-write_log "zoxide"
-yay -S --noconfirm zoxide
-
-write_log "croc"
-yay -S --noconfirm croc
-
-write_log "curl"
-yay -S --noconfirm curl
-
-write_log "dua-cli"
-yay -S --noconfirm dua-cli
-
-write_log "duf"
-yay -S --noconfirm duf
-
-write_log "procs"
-yay -S --noconfirm procs
-
-write_log "tre-command"
-yay -S --noconfirm tre-command
-
-write_log "neofetch"
-yay -S --noconfirm neofetch
-
-write_log "fastfetch"
-yay -S --noconfirm fastfetch
-
-write_log "bottom"
-yay -S --noconfirm bottom
-
-write_log "git-delta"
-yay -S --noconfirm git-delta
-
-write_log "sd"
-yay -S --noconfirm sd
-
-write_log "tealdeer"
-yay -S --noconfirm tealdeer
-
-write_log "rslsync"
-yay -S --noconfirm rslsync
-
-write_log "kime"
-yay -S --noconfirm kime
-
-write_log "jetbrains-toolbox"
-yay -S --noconfirm jetbrains-toolbox
-
-write_log "go"
-yay -S --noconfirm go
-
-write_log "hyprland-git"
-yay -S --noconfirm hyprland-git
-
-write_log "hyprlock-git"
-yay -S --noconfirm hyprlock-git
-
-write_log "xdg-desktop-portal-hyprland-git"
-yay -S --noconfirm xdg-desktop-portal-hyprland-git
-
-write_log "swaync-git"
-yay -S --noconfirm swaync-git
-
-write_log "cliphist"
-yay -S --noconfirm cliphist
-
-write_log "playerctl-git"
-yay -S --noconfirm playerctl-git
-
-write_log "grim-git"
-yay -S --noconfirm grim-git
-
-write_log "slurp-git"
-yay -S --noconfirm slurp-git
-
-write_log "jq"
-yay -S --noconfirm jq
-
-write_log "wf-recorder-git"
-yay -S --noconfirm wf-recorder-git
-
-write_log "brightnessctl-git"
-yay -S --noconfirm brightnessctl-git
-
-write_log "wine"
-yay -S --noconfirm wine
-
-write_log "telegram-desktop"
-yay -S --noconfirm telegram-desktop
-
-write_log "figma-linux-bin"
-yay -S --noconfirm figma-linux-bin
-
-write_log "notion-app-electron"
-yay -S --noconfirm notion-app-electron
+# Install packages
+center_text "Install packages"
+yay -S --noconfirm \
+    keyd \
+    gdm \
+    google-chrome \
+    obsidian \
+    discord \
+    fish \
+    ttf-jetbrains-mono-nerd \
+    tofi \
+    eww \
+    bat \
+    kitty \
+    fd \
+    fzf \
+    lsd \
+    neovim \
+    neovide \
+    ripgrep \
+    starship \
+    zoxide \
+    croc \
+    curl \
+    dua-cli \
+    duf \
+    procs \
+    tre-command \
+    neofetch \
+    fastfetch \
+    bottom \
+    git-delta \
+    sd \
+    tealdeer \
+    rslsync \
+    kime-git \
+    jetbrains-toolbox \
+    go \
+    hyprland-git \
+    hyprlock-git \
+    xdg-desktop-portal-hyprland-git \
+    swaync-git \
+    cliphist \
+    playerctl-git \
+    grim-git \
+    slurp-git \
+    jq \
+    wf-recorder-git \
+    brightnessctl-git \
+    wine-stable \
+    telegram-desktop \
+    figma-linux-bin \
+    notion-app-electron
 
 write_log "Done"
 
@@ -226,13 +134,17 @@ sudo systemctl enable keyd.service
 sudo systemctl start keyd.service
 # TODO: Add keyd configuration
 
-
 # Install resilio-sync
 center_text "Setup resilio-sync"
 mkdir -p ~/.config/rslsync ~/.sync
 gh repo clone lewohy/rslsync ~/.config/rslsync
 systemctl enable --user rslsync.service
 systemctl start --user rslsync.service
+write_log "Done"
+
+# Enable gdm
+center_text "Enable gdm"
+sudo systemctl enable gdm.service
 write_log "Done"
 
 # timedatectl(듀얼부팅시 윈도우-리눅스 시간안맞는 문제 해결)
